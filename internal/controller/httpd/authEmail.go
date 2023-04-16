ee8717493bf727f834204d49e9ec62a0fbf0aff3
asavor <74147411+asavor@users.noreply.github.com>
2023-04-16 02:20:09 +0100

    added form validation

--- internal/controller/httpd/authEmail.go
+++ internal/controller/httpd/authEmail.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/vyroai/VyroAI/commons/api/response"
 	"github.com/vyroai/VyroAI/commons/errors"
+	"github.com/vyroai/VyroAI/commons/helper/formSantanizer"
 	"github.com/vyroai/VyroAI/commons/jwt"
 )
 
@@ -53,6 +54,19 @@ func (s *WebServiceHttpServer) register(c *fiber.Ctx) error {
 
 	if err := c.BodyParser(&register); err != nil {
 		response.ErrorJson(c, 401, err.Error())
+		return nil
+	}
+
+	if !formSantanizer.IsValidEmail(register.Email) {
+		response.ErrorJson(c, 400, "invalid payload")
+		return nil
+	}
+	if !formSantanizer.IsValidUsername(register.Username) {
+		response.ErrorJson(c, 400, "invalid payload")
+		return nil
+	}
+	if !formSantanizer.IsPasswordValid(register.Password) {
+		response.ErrorJson(c, 400, "invalid payload")
 		return nil
 	}
 
